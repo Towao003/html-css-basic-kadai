8e698c7c1570521f503200821eea18cf32390d92
Towao003 <163400144+Towao003@users.noreply.github.com>
2024-05-06 11:56:15 +0900

    Add files via upload

---
+++ kdai_014/square.js
@@ -0,0 +1,8 @@
+const calculateSquare = (number) => {
+    const result = number ** 2;
+    console.log("計算結果:", result);
+    return result;
+};
+
+const inputNumber = 10; 
+const squaredResult = calculateSquare(inputNumber);
