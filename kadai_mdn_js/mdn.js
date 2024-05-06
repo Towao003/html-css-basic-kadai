d280e554db3f638edf877e22370910796b95e7de
Towao003 <163400144+Towao003@users.noreply.github.com>
2024-05-06 10:54:45 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,8 @@
+const currentDate = new Date();
+    
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1; 
+    const day = currentDate.getDate();
+    
+
+    console.log(`年: ${year}, 月: ${month}, 日付: ${day}`);
