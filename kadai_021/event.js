01ad8717554265d130a152f642a1b91e43d80683
Towao003 <163400144+Towao003@users.noreply.github.com>
2024-05-10 17:12:46 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn");
+    const text = document.getElementById("text");
+  
+    btn.addEventListener("click", () => {
+      setTimeout(() => {
+        text.textContent = "ボタンがクリックされました";
+      }, 2000);
+    });
+  });
+  
